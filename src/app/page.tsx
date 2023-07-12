ea4040d9c1cb35e105d6a5b0c1fa62af3877ce00
dinkleva <dinkleva@gmail.com>
2023-07-13 00:57:55 +0530

    Making Responsive component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="max-w-xl mx-auto p-4">
       <h1 className="text-6xl font-bold mt-64 mb-4 text-blue-600">Hello there</h1>
-      <p className="text-2xl font-bold mt-4 text-blue-200">Please go to this <a href="/mypage" className="text-red-300">App</a> link</p>
+      <p className="text-2xl font-bold mt-4 text-blue-200">Please go to this <a href="/mypage" className="text-red-300 underline">App</a> link</p>
     </div>
   )
 }
